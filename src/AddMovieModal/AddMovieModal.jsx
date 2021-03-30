0badba23030847134daaa3e8fe65d17f0b7cc227
Marton Soltesz-Nagy <marton_soltesz-nagy@epam.com>
2021-03-30 17:48:57 +0200

    exercise 9 addmodal testing in progress

--- src/AddMovieModal/AddMovieModal.jsx
+++ src/AddMovieModal/AddMovieModal.jsx
@@ -85,44 +85,44 @@ const AddMovieModal = React.forwardRef((props, ref) => {
                 <TextField id="title" name="title" type="text" label="Title"
                     onChange={formik.handleChange} value={formik.values.title} />
             </div>
-            {formik.errors.title ? <div className="error">{formik.errors.title}</div> : null}
+            {formik.errors.title ? <div id="title-error" className="error">{formik.errors.title}</div> : null}
 
             <div>
                 <TextField label="Release date" InputLabelProps={{ shrink: true }} id="release_date"
                     name="release_date" type="date" onChange={formik.handleChange} value={formik.values.release_date} />
             </div>
-            {formik.errors.release_date ? <div className="error">{formik.errors.release_date}</div> : null}
+            {formik.errors.release_date ? <div id="release_date-error" className="error">{formik.errors.release_date}</div> : null}
 
             <div>
                 <TextField label="Movie URL" id="poster_path" name="poster_path" type="text"
                     onChange={formik.handleChange} value={formik.values.poster_path}
                 />
             </div>
-            {formik.errors.poster_path ? <div className="error">{formik.errors.poster_path}</div> : null}
+            {formik.errors.poster_path ? <div id="poster_path-error" className="error">{formik.errors.poster_path}</div> : null}
 
             <div>
                 <FormControl>
                     <InputLabel>Genres</InputLabel>
                     <Select id="genres" name="genres" value={formik.values.genres} onChange={formik.handleChange} multiple>
                         {genres.map(genreItem => (
-                            <MenuItem key={genreItem} value={genreItem}>{genreItem}</MenuItem>
+                            <MenuItem id={genreItem} key={genreItem} value={genreItem}>{genreItem}</MenuItem>
                         ))}
                     </Select>
                 </FormControl>
             </div>
-            {formik.errors.genres ? <div className="error">{formik.errors.genres}</div> : null}
+            {formik.errors.genres ? <div id="genres-error" className="error">{formik.errors.genres}</div> : null}
 
             <div>
                 <TextField label="Overview" id="overview" name="overview" type="text"
                     multiline rows={3} rowsMax={10} onChange={formik.handleChange} value={formik.values.overview} />
             </div>
-            {formik.errors.overview ? <div className="error">{formik.errors.overview}</div> : null}
+            {formik.errors.overview ? <div id="overview-error" className="error">{formik.errors.overview}</div> : null}
 
             <div>
                 <TextField label="Runtime" id="runtime" name="runtime" type="number"
                     onChange={formik.handleChange} value={formik.values.runtime} />
             </div>
-            {formik.errors.runtime ? <div className="error">{formik.errors.runtime}</div> : null}
+            {formik.errors.runtime ? <div id="runtime-error" className="error">{formik.errors.runtime}</div> : null}
 
             <div>
                 <Button variant="contained" color="primary" className="AddMovieFormButtons" type="submit"
--- src/AddMovieModal/AddMovieModal.test.js
+++ src/AddMovieModal/AddMovieModal.test.js
@@ -4,8 +4,16 @@ import AddMovieModal from './AddMovieModal';
 import { Provider } from "react-redux";
 import { createStore, combineReducers } from 'redux';
 import movieReducer from '../store/reducers/movie';
-import { render } from "react-dom";
-import { act } from '@testing-library/react';
+
+const testMovie = {
+    title: 'title1',
+    poster_path: 'img',
+    release_date: '2000-01-01',
+    overview: 'asd',
+    runtime: 90,
+    genres: ['Action'],
+    tagline: 'asd'
+};
 
 describe("AddMovieModal test", () => {
     let store;
@@ -18,39 +26,106 @@ describe("AddMovieModal test", () => {
                 movie: movieReducer
             })
         );
-
         ref = React.createRef();
-        wrapper = shallow(
+        wrapper = mount(
             <Provider store={store} >
                 <AddMovieModal ref={ref} />
             </Provider>
         )
     });
 
+    afterEach(() => {
+        wrapper.unmount();
+    })
+
     it('renders correctly', () => {
         expect(wrapper).toBeTruthy();
     });
 
-    it('asdasd', async () => {
-        let asd = document.createElement('div');
-        // store.setState({movie: { ...}})
-        await act(async () => {
-            render(<Provider store={store} >
-                <AddMovieModal ref={ref} />
-            </Provider>, asd)
-        });
-
-        const submitButton = asd.querySelector('button[id="AddMovieSubmit"]');
-        //wrapper.findByLabel('#AddMovieSubmit').length;
-        // console.log(submitButton)
-        await submitButton.dispatchEvent(new MouseEvent('click'));
-
-        // await act(async () => {
-        //     submitButton.dispatchEvent(new MouseEvent("click", { bubbles: true }));
-        // });
-        const errorMessages = asd.querySelector('div[class="error"]');
-        console.log(errorMessages)
-
+    it('error messages appear', () => {
+        wrapper.find('button#AddMovieSubmit').simulate('click');
+        const errorMessages = wrapper.find('div.error');
         expect(errorMessages).toBeTruthy();
+    });
+
+    it('should runtime message appear if empty', () => {
+        wrapper.find('input#title').simulate('keydown', { key: 'a' });
+        wrapper.find('input#poster_path').simulate('keydown', { key: 'a' });
+        wrapper.find('textarea#overview').simulate('keydown', { key: 'a' });
+        wrapper.find('input#release_date')
+            .simulate('keydown', { key: '2' })
+            .simulate('keydown', { key: '0' })
+            .simulate('keydown', { key: '0' })
+            .simulate('keydown', { key: '0' })
+            .simulate('keydown', { key: '-' })
+            .simulate('keydown', { key: '0' })
+            .simulate('keydown', { key: '1' })
+            .simulate('keydown', { key: '-' })
+            .simulate('keydown', { key: '0' })
+            .simulate('keydown', { key: '1' });
+        wrapper.find('input#runtime').simulate('keydown', { key: '9' });
+        wrapper.find('div#genres').simulate('change', { target: { value: ['Action'] } });
+
+        wrapper.find('button#AddMovieSubmit').simulate('click');
+        expect(wrapper.find('div.error').exists()).toBeFalsy();
+    });
+
+    it('should fill inputs with data when editing', () => {
+        store.getState().movie.editableMovie = { ...testMovie };
+        const component = mount(
+            <Provider store={store}>
+                <AddMovieModal ref={ref} movieDetail={testMovie} />
+            </Provider>
+        );
+
+        expect(component.find('input#title').html()).toContain('value="title1"');
+        expect(component.find('input#release_date').html()).toContain('value="2000-01-01"');
+        expect(component.find('textarea#overview').html()).toContain('asd');
+        expect(component.find('input#runtime').html()).toContain('value="90"');
+        expect(component.find('div#genres').html()).toContain('Action');
+        component.unmount();
+    });
+
+    it('should not display error messagess if not submitted', () => {
+        wrapper.find('input#title').simulate('keydown', { key: 'a' });
+        expect(wrapper.find('div.error').exists()).toBe(false);
+    });
+
+    fit('should call onAdd when submitting new movie', () => {
+        const asd = {
+            onAdd: jest.fn(),
+            onEdit: jest.fn(),
+            ref: ref
+        }
+        const component = mount(
+            <Provider store={store}>
+                <AddMovieModal {...asd} />
+            </Provider>
+        );
+        // addMovieModal.setProps({ref: ref, onAdd: onAdd})
+        console.log(component)
+
+        component.find('input#title').simulate('keydown', { key: 'a' });
+        component.find('input#poster_path').simulate('keydown', { key: 'a' });
+        component.find('textarea#overview').simulate('keydown', { key: 'a' });
+        component.find('input#release_date')
+            .simulate('keydown', { key: '2' })
+            .simulate('keydown', { key: '0' })
+            .simulate('keydown', { key: '0' })
+            .simulate('keydown', { key: '0' })
+            .simulate('keydown', { key: '-' })
+            .simulate('keydown', { key: '0' })
+            .simulate('keydown', { key: '1' })
+            .simulate('keydown', { key: '-' })
+            .simulate('keydown', { key: '0' })
+            .simulate('keydown', { key: '1' });
+        component.find('input#runtime').simulate('keydown', { key: '9' });
+        component.find('div#genres').simulate('change', { target: { value: ['Action'] } });
+
+        component.find('button#AddMovieSubmit').simulate('click');
+        // component.instance().onAdd();
+
+        expect(asd.onEdit).toBeCalled();
+        expect(asd.onAdd).toBeCalled();
     });
 });
